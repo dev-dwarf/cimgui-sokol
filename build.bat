848536a2285dc8f3ce31a688d4cb24ecfac4737f
dev_dwarf <logan.c.forman@gmail.com>
2024-07-29 23:36:00 -0600

    get compiling with simple .bat file

---
+++ build.bat
@@ -0,0 +1,18 @@
+@echo off
+IF NOT EXIST build mkdir build
+
+pushd build
+
+set SOURCES= ..\cimgui\cimgui.cpp ..\cimgui\imgui\imgui*.cpp ..\sokol\sokol.c
+set INCLUDES=/I..\cimgui\ /I..\cimgui\imgui /I..\sokol /I..\..
+set LIBS=
+set NOWARN=-wd4201 -wd4100 -wd4189 -wd4244 -wd4456 -wd4457 -wd4245
+set FLAGS=/nologo /utf-8 /W4 /Oi /GR- /FC %NOWARN%
+set DEF=/D IMGUI_DISABLE_OBSOLETE_FUNCTIONS=1 /D IMGUI_IMPL_API=extern\t\"C\"\t__declspec\(dllexport\) /D ImTextureID=ImU64 /D UNICODE /D _UNICODE
+
+IF "%1"=="full" cl /c %FLAGS% %INCLUDES% %DEF% %SOURCES% /link %LIBS%
+
+rm -f demo.obj
+cl %FLAGS% %INCLUDES% %DEF% ..\demo.c /link /NOEXP /NOIMPLIB %LIBS% *.obj /out:demo.exe
+
+popd
